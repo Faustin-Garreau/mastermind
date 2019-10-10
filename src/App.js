9304c80547341ffae0b011824910889e1172f506
Fenidorn <tbeniere@edenschool.fr>
2019-10-10 15:54:01 +0200

    ajout Header react sans compo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './framework.css';
+import logo from "./img/logo.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App h-screen">
+      <div className="Header flex flex-col justify-center items-center">
+        <div className="logo flex justify-center items-center">
+          <img id="logo" src={logo} alt="logo"/>
+        </div>
+        <nav className="nav">
+          <ul className="nav-ul flex">
+            <li className="p-5">
+              Scores
+            </li>
+            <li className="p-5">
+              Règles
+            </li>
+            <li className="p-5">
+              Contact
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="Main">
+        <div className="plateau">
+          
+        </div>
+      </div>
     </div>
   );
 }
