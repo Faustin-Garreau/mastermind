ec9b49367a2e5f255fdef22e8241c3014310c638
Fenidorn <tbeniere@edenschool.fr>
2019-10-10 17:36:02 +0200

    ajout footer + ajustement taille

--- src/App.js
+++ src/App.js
@@ -24,30 +24,16 @@ function App() {
           </ul>
         </nav>
       </div>
-      <div className="Main p-5">
+      <div className="Main p-3">
         <div className="plateau">
           <div className="master-row w-full">
-            <div className="master-col w-2/3">
+            <div className="master-col  justify-around w-2/3">
               <div></div>
               <div></div>
               <div></div>
               <div></div>
             </div>
-            <div className="master-col w-1/3">
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-            </div>
-          </div>
-          <div className="master-row w-full">
-            <div className="master-col w-2/3">
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-            </div>
-            <div className="master-col w-1/3">
+            <div className="master-col2 w-1/3">
               <div></div>
               <div></div>
               <div></div>
@@ -55,27 +41,13 @@ function App() {
             </div>
           </div>
           <div className="master-row w-full">
-            <div className="master-col w-2/3">
+            <div className="master-col  justify-around w-2/3">
               <div></div>
               <div></div>
               <div></div>
               <div></div>
             </div>
-            <div className="master-col w-1/3">
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-            </div>
-          </div>
-          <div className="master-row w-full">
-            <div className="master-col w-2/3">
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-            </div>
-            <div className="master-col w-1/3">
+            <div className="master-col2 w-1/3">
               <div></div>
               <div></div>
               <div></div>
@@ -83,27 +55,13 @@ function App() {
             </div>
           </div>
           <div className="master-row w-full">
-            <div className="master-col w-2/3">
+            <div className="master-col  justify-around w-2/3">
               <div></div>
               <div></div>
               <div></div>
               <div></div>
             </div>
-            <div className="master-col w-1/3">
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-            </div>
-          </div>
-          <div className="master-row w-full">
-            <div className="master-col w-2/3">
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-            </div>
-            <div className="master-col w-1/3">
+            <div className="master-col2 w-1/3">
               <div></div>
               <div></div>
               <div></div>
@@ -111,27 +69,13 @@ function App() {
             </div>
           </div>
           <div className="master-row w-full">
-            <div className="master-col w-2/3">
+            <div className="master-col  justify-around w-2/3">
               <div></div>
               <div></div>
               <div></div>
               <div></div>
             </div>
-            <div className="master-col w-1/3">
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-            </div>
-          </div>
-          <div className="master-row w-full">
-            <div className="master-col w-2/3">
-              <div></div>
-              <div></div>
-              <div></div>
-              <div></div>
-            </div>
-            <div className="master-col w-1/3">
+            <div className="master-col2 w-1/3">
               <div></div>
               <div></div>
               <div></div>
@@ -139,13 +83,13 @@ function App() {
             </div>
           </div>
           <div className="master-row w-full">
-            <div className="master-col w-2/3">
+            <div className="master-col  justify-around w-2/3">
               <div></div>
               <div></div>
               <div></div>
               <div></div>
             </div>
-            <div className="master-col w-1/3">
+            <div className="master-col2 w-1/3">
               <div></div>
               <div></div>
               <div></div>
@@ -153,19 +97,90 @@ function App() {
             </div>
           </div>
           <div className="master-row w-full">
-            <div className="master-col w-2/3">
+            <div className="master-col  justify-around w-2/3">
               <div></div>
               <div></div>
               <div></div>
               <div></div>
             </div>
-            <div className="master-col w-1/3">
+            <div className="master-col2 w-1/3">
               <div></div>
               <div></div>
               <div></div>
               <div></div>
             </div>
           </div>
+          <div className="master-row w-full">
+            <div className="master-col  justify-around w-2/3">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div className="master-col2 w-1/3">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
+          <div className="master-row w-full">
+            <div className="master-col  justify-around w-2/3">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div className="master-col2 w-1/3">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
+          <div className="master-row w-full">
+            <div className="master-col  justify-around w-2/3">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div className="master-col2 w-1/3">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
+          <div className="master-row w-full">
+            <div className="master-col  justify-around w-2/3">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div className="master-col2 w-1/3">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="Footer p-1 flex justify-around items-center">
+        <div className="palette flex w-2/3">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="action flex w-1/3">
+          <div className="reset"><i class="fas fa-undo"></i></div>
+          <div className="go"></div>
         </div>
       </div>
     </div>
