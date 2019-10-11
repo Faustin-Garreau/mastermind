aa27d524ddbbc2655cc55267412d1b6cbea3fe5d
Fenidorn <tbeniere@edenschool.fr>
2019-10-11 15:30:19 +0200

    ajout main fait

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import logo from "./img/logo.png";
 function App() {
   return (
     <div className="App">
-      <header className="bg-red-500">
-        <div>
-          <img id="logo" src={logo} alt="logo"/>
+      <header className="flex flex-col justify-center items-center">
+        <div className="logo">
+          <img className="mx-auto" id="logo" src={logo} alt="logo"/>
         </div>
         <nav className="nav">
-          <ul className="nav-ul">
+          <ul className="nav-ul flex">
             <li>
               Scores
             </li>
@@ -23,7 +23,190 @@ function App() {
           </ul>
         </nav>
       </header>
-      
+      <main>
+        <div className="mastermind flex flex-col justfiy-center items-center">
+          <div className="master-row flex">
+            <div className="master-col flex items-center">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div className="master-col2">
+              <div className="master-col2-top flex">
+                <div></div>
+                <div></div>
+              </div>
+              <div className="master-col2-bot flex">
+                <div></div>
+                <div></div>
+              </div>
+            </div>
+          </div>
+          <div className="master-row flex">
+            <div className="master-col flex items-center">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div className="master-col2">
+              <div className="master-col2-top flex ">
+                <div></div>
+                <div></div>
+              </div>
+              <div className="master-col2-bot flex">
+                <div></div>
+                <div></div>
+              </div>
+            </div>
+          </div>
+          <div className="master-row flex">
+            <div className="master-col flex items-center">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div className="master-col2">
+              <div className="master-col2-top flex">
+                <div></div>
+                <div></div>
+              </div>
+              <div className="master-col2-bot flex">
+                <div></div>
+                <div></div>
+              </div>
+            </div>
+          </div>
+          <div className="master-row flex">
+            <div className="master-col flex items-center">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div className="master-col2">
+              <div className="master-col2-top flex ">
+                <div></div>
+                <div></div>
+              </div>
+              <div className="master-col2-bot flex">
+                <div></div>
+                <div></div>
+              </div>
+            </div>
+          </div>
+          <div className="master-row flex">
+            <div className="master-col flex items-center">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div className="master-col2">
+              <div className="master-col2-top flex">
+                <div></div>
+                <div></div>
+              </div>
+              <div className="master-col2-bot flex">
+                <div></div>
+                <div></div>
+              </div>
+            </div>
+          </div>
+          <div className="master-row flex">
+            <div className="master-col flex items-center">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div className="master-col2">
+              <div className="master-col2-top flex ">
+                <div></div>
+                <div></div>
+              </div>
+              <div className="master-col2-bot flex">
+                <div></div>
+                <div></div>
+              </div>
+            </div>
+          </div>
+          <div className="master-row flex">
+            <div className="master-col flex items-center">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div className="master-col2">
+              <div className="master-col2-top flex items-center">
+                <div></div>
+                <div></div>
+              </div>
+              <div className="master-col2-bot flex">
+                <div></div>
+                <div></div>
+              </div>
+            </div>
+          </div>
+          <div className="master-row flex">
+            <div className="master-col flex items-center">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div className="master-col2">
+              <div className="master-col2-top flex items-center">
+                <div></div>
+                <div></div>
+              </div>
+              <div className="master-col2-bot flex">
+                <div></div>
+                <div></div>
+              </div>
+            </div>
+          </div>
+          <div className="master-row flex">
+            <div className="master-col flex items-center">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div className="master-col2">
+              <div className="master-col2-top flex items-center">
+                <div></div>
+                <div></div>
+              </div>
+              <div className="master-col2-bot flex">
+                <div></div>
+                <div></div>
+              </div>
+            </div>
+          </div>
+          <div className="master-row flex">
+            <div className="master-col flex items-center">
+              <div></div>
+              <div></div>
+              <div></div>
+              <div></div>
+            </div>
+            <div className="master-col2">
+              <div className="master-col2-top flex">
+                <div></div>
+                <div></div>
+              </div>
+              <div className="master-col2-bot flex">
+                <div></div>
+                <div></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
